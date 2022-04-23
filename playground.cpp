5ec013658f977fed60e476e5b17c0a51078dbe2e
vanilla milkshake <vanilla.milkshake.for.life@gmail.com>
2022-04-24 01:43:19 +0300

    Simplify playground.cpp

--- playground.cpp
+++ playground.cpp
@@ -1,35 +1,16 @@
 #include <concepts>
 
+// clang-format off
 template <typename T>
 concept Boolean = std::convertible_to<T, bool>;
 
-template <Boolean... booleans> constexpr bool NOT(booleans... bs) {
-  return !(bs, ...);
-}
-
-template <Boolean... booleans> constexpr bool AND(booleans... bs) {
-  return (... && bs);
-}
-
-template <Boolean... booleans> constexpr bool NAND(booleans... bs) {
-  return NOT(AND(bs...));
-}
-
-template <Boolean... booleans> bool constexpr OR(booleans... bs) {
-  return (... || bs);
-}
-
-template <Boolean... booleans> constexpr bool NOR(booleans... bs) {
-  return NOT(OR(bs...));
-}
-
-template <Boolean... booleans> constexpr bool XOR(booleans... bs) {
-  return (bs ^ ...);
-}
-
-template <Boolean... booleans> constexpr bool XNOR(booleans... bs) {
-  return NOT((bs ^ ...));
-}
+consteval bool NOT  (const Boolean auto... bs) { return !(bs, ...);      }
+consteval bool AND  (const Boolean auto... bs) { return (... && bs);     }
+consteval bool NAND (const Boolean auto... bs) { return NOT(AND(bs...)); }
+consteval bool OR   (const Boolean auto... bs) { return (... || bs);     }
+consteval bool NOR  (const Boolean auto... bs) { return NOT(OR(bs...));  }
+consteval bool XOR  (const Boolean auto... bs) { return (bs ^ ...);      }
+consteval bool XNOR (const Boolean auto... bs) { return NOT((bs ^ ...)); }
 
 int main() {
   constexpr bool a = true, b = true;
